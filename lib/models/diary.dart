d0c919b7fa25f130ddc62c560b8a4d3c45b4b179
kahyeon17 <kahyun0817@naver.com>
2024-03-31 18:29:36 +0900

    Modify start_screen file

--- lib/models/diary.dart
+++ lib/models/diary.dart
@@ -3,20 +3,21 @@ import 'package:intl/intl.dart';
 
 final formatter = DateFormat.yMMMd();
 
-class Diary{
+class Diary {
   Diary({
     required this.date,
     required this.text,
   });
-  
-  
+
   String text;
   final DateTime date;
-
 
   String get formattedDate {
     return formatter.format(date);
   }
+
+  String getDayofMonth() {
+    return date.day.toString();
+  }
   //get을 사용하는 것은 보안의 위험 때문인가? var를 사용해서 다른 변수에 저장하기 귀찮아서?
 }
-
--- lib/widgets/diary_show.dart
+++ lib/widgets/diary_show.dart
@@ -10,41 +10,58 @@ class DiaryShow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/images/StarryNight.jpg',
-            height: 400,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Row(
-            children: [
-              Text(
-                todayDiary.formattedDate,
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
+      child: Container(
+        // decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(15),
+        //     border: Border.all(color: Colors.white)),
+        child: Column(
+          children: [
+            Text(
+              textAlign: TextAlign.left,
+              todayDiary.getDayofMonth(),
+              style: const TextStyle(
+                fontSize: 45,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(
-                width: 30,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              todayDiary.text,
+              style: const TextStyle(
+                color: Colors.white,
               ),
-              Container(
-                  width: 200,
-                  child: const Divider(
-                      color: Color.fromARGB(255, 240, 226, 225),
-                      thickness: 2.0)),
-            ],
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(todayDiary.text),
-        ],
+            ),
+            // Image.asset(
+            //   'assets/images/StarryNight.jpg',
+            //   height: 400,
+            // ),
+            // const SizedBox(
+            //   height: 16,
+            // ),
+            // Row(
+            //   children: [
+            //     // Text(
+            //     //   todayDiary.formattedDate,
+            //     //   style: const TextStyle(
+            //     //     fontSize: 20,
+            //     //   ),
+            //     // ),
+            //     const SizedBox(
+            //       width: 30,
+            //     ),
+            //     Container(
+            //         width: 200,
+            //         child: const Divider(
+            //             color: Color.fromARGB(255, 240, 226, 225),
+            //             thickness: 2.0)),
+            //   ],
+            // ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
