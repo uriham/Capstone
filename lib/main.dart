74e69d61cb237bf39102a6b01cca5a901861205c
kimshinwoo <tlsdn67@naver.com>
2024-04-10 16:25:15 +0900

    okay누르면 api통신 후 print문으로 나옴

--- lib/main.dart
+++ lib/main.dart
@@ -24,44 +24,7 @@ final theme = ThemeData.dark().copyWith(
 
 void main() async{
   /*
-  OpenAI.apiKey = Env.apiKey;
-  final systemMessage = OpenAIChatCompletionChoiceMessageModel(
-  content: [
-    OpenAIChatCompletionChoiceMessageContentItemModel.text(
-      "return any message you are given as JSON.",
-    ),
-  ],
-  role: OpenAIChatMessageRole.assistant,
-);
-
-  // the user message that will be sent to the request.
- final userMessage = OpenAIChatCompletionChoiceMessageModel(
-   content: [
-     OpenAIChatCompletionChoiceMessageContentItemModel.text(
-       "Hello, I am a chatbot created by OpenAI. How are you today?",
-     ),
-    
-   ],
-   role: OpenAIChatMessageRole.user,
- );
-
-// all messages to be sent.
-final requestMessages = [
-  systemMessage,
-  userMessage,
-];
-
-// the actual request.
-OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
-  model: "gpt-3.5-turbo-1106",
-  responseFormat: {"type": "json_object"},
-  seed: 6,
-  messages: requestMessages,
-  temperature: 0.2,
-  maxTokens: 500,
-);
-
-print(chatCompletion.choices.first.message); 
+ 
 */
 
 
--- lib/widgets/diary_textfield.dart
+++ lib/widgets/diary_textfield.dart
@@ -2,6 +2,8 @@ import 'package:capstone/models/diary.dart';
 import 'package:capstone/providers/diary_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:capstone/env.dart';
+import 'package:dart_openai/dart_openai.dart';
 
 class DiaryTextField extends ConsumerWidget {
   DiaryTextField({
@@ -10,7 +12,50 @@ class DiaryTextField extends ConsumerWidget {
   });
 
   final Diary todayDiary;
+  var chatText = '-------Loading-------';
   final TextEditingController _textEditingController = TextEditingController();
+
+  Future _openai() async {
+    OpenAI.apiKey = Env.apiKey;
+    final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          "한국말로 대답해",
+        ),
+      ],
+      role: OpenAIChatMessageRole.assistant,
+    );
+
+    // the user message that will be sent to the request.
+    final userMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          "안녕 오늘의 기분은 어때?",
+        ),
+      ],
+      role: OpenAIChatMessageRole.user,
+    );
+
+// all messages to be sent.
+    final requestMessages = [
+      systemMessage,
+      userMessage,
+    ];
+
+// the actual request.
+    OpenAIChatCompletionModel chatCompletion =
+        await OpenAI.instance.chat.create(
+      model: "gpt-3.5-turbo-1106",
+      seed: 6,
+      messages: requestMessages,
+      temperature: 0.2,
+      maxTokens: 500,
+    );
+    print(chatCompletion.choices.first.message); // ...
+    print(chatCompletion.systemFingerprint); // ...
+    print(chatCompletion.usage.promptTokens); // ...
+    print(chatCompletion.id);
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -39,8 +84,9 @@ class DiaryTextField extends ConsumerWidget {
                       ElevatedButton(
                         child: Text('Okay'),
                         onPressed: () {
+                          _openai();
                           Navigator.of(context).pop();
-                          ref
+                          var a = ref
                               .read(diaryProvider.notifier)
                               .editTodayDiary(_textEditingController.text);
                         },
