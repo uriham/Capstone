f12a0871ae4e7f140466200c8154fee711780dcd
kimshinwoo <tlsdn67@naver.com>
2024-06-09 16:25:27 +0900

    로딩화면 밑 url sysmessage 수정

--- lib/widgets/loading_text.dart
+++ lib/widgets/loading_text.dart
@@ -20,10 +20,20 @@ class _LoadingTextState extends State<LoadingText> {
         ),
         child: AnimatedTextKit(
           animatedTexts: [
-            TypewriterAnimatedText('Discipline is the best tool'),
-            TypewriterAnimatedText('Design first, then code'),
-            TypewriterAnimatedText('Do not patch bugs out, rewrite them'),
-            TypewriterAnimatedText('Do not test bugs out, design them out'),
+            FadeAnimatedText('마오먀아는 봄베이라는 고양이 품종입니다.',
+                duration: const Duration(seconds: 5)),
+            FadeAnimatedText(
+              '본 Dizrizm은 약 3개월만에 제작되었습니다.',
+              duration: const Duration(seconds: 5),
+            ),
+            FadeAnimatedText(
+              '이상의 이름은 조선총독부에서 건축기사로 일했을 당시 인부가 그의 성을 김씨가 아닌 이씨로 착각하여 이상이라고 불렸다는 설이 있습니다.',
+              duration: const Duration(seconds: 5),
+            ),
+            FadeAnimatedText(
+              '밤에 늦게 자면 배고픈 이유는 그렘린이라는 호르몬 때문입니다.',
+              duration: const Duration(seconds: 5),
+            ),
           ],
           onTap: () {
             print("Tap Event");
