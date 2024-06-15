03b07cb38d4fcc274ab6b97a3ce9d4638c073d2c
kahyeon17 <kahyun0817@naver.com>
2024-06-15 22:53:30 +0900

    edit 저장 방법 수정

--- lib/screens/add_edit_book.dart
+++ lib/screens/add_edit_book.dart
@@ -113,7 +113,6 @@ class _AddEditBookScreenState extends ConsumerState<AddEditBookScreen> {
                       ],
                     );
                   });
-              //Navigator.pop(context);
             },
           ),
           title: widget.diary == null
